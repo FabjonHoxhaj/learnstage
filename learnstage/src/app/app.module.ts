52e514c50dc55ac14a455f0e7d4498e00cd50b9f
U-DESKTOP-SFCGEHP\mikro <fahox1991@gmail.com>
2022-02-19 17:03:04 +0100

    added Hashtags on Homepage

--- learnstage/src/app/app.module.ts
+++ learnstage/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MainComponent } from './main/main.component';
+import { AsideTagsComponent } from './aside-tags/aside-tags.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    MainComponent
+    MainComponent,
+    AsideTagsComponent
   ],
   imports: [
     BrowserModule,
