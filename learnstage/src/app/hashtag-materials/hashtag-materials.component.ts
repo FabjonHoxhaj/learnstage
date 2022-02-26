abb0a833115073389561dc0e8de064c3df57a7e3
U-DESKTOP-SFCGEHP\mikro <fahox1991@gmail.com>
2022-02-26 01:19:26 +0100

    firebase files prepared

--- learnstage/src/app/hashtag-materials/hashtag-materials.component.ts
+++ learnstage/src/app/hashtag-materials/hashtag-materials.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from '../upload.service';
+import { FileUpload } from 'src/app/models/file-upload.model';
 
 @Component({
   selector: 'app-hashtag-materials',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HashtagMaterialsComponent implements OnInit {
 
-  constructor() { }
-
+  selectedFiles?: FileList;
+  currentFileUpload?: FileUpload;
+  percentage = 0;
+  constructor(private uploadService: FileUploadService) { }
   ngOnInit(): void {
+  }
+  selectFile(event: any): void {
+    this.selectedFiles = event.target.files;
+  }
+  upload(): void {
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      this.selectedFiles = undefined;
+      if (file) {
+        this.currentFileUpload = new FileUpload(file);
+        this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(
+          percentage => {
+            this.percentage = Math.round(percentage ? percentage : 0);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
+    }
   }
 
 }
