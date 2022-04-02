05056a2d4538506a1b6648cd62e7b4d1f6d26ccd
Fabjon Hoxhaj <fahox1991@gmail.com>
2022-04-02 12:19:21 +0200

    hostbinding and hostlistener added

--- learnstage/src/app/star.directive.ts
+++ learnstage/src/app/star.directive.ts
@@ -1,9 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStar]'
 })
 export class StarDirective {
+  private normalColor = "#D3D3D3";
+  
+  @HostBinding("style.color") color = this.normalColor
+
+  @HostBinding("style.fontSize") fontSize = "19px"
+
+  @HostListener("mouseenter") mouseEnter() {
+    this.color = "#deb217"
+  }
+
+  @HostListener("mouseleave") mouseLeave() {
+    this.color = this.normalColor
+  }
+
+  @HostListener("click") onClick() {
+    this.normalColor = "orange";  
+    this.color = this.normalColor;
+  }
 
   constructor() { }
 
