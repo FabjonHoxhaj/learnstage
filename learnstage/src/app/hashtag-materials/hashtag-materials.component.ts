9ace9c2e727897afcd7747ab729069b62823c759
Fabjon Hoxhaj <fahox1991@gmail.com>
2022-05-06 17:18:34 +0200

    initial commit

--- learnstage/src/app/hashtag-materials/hashtag-materials.component.ts
+++ learnstage/src/app/hashtag-materials/hashtag-materials.component.ts
@@ -17,6 +17,7 @@ export class HashtagMaterialsComponent implements OnInit {
   stars = [1, 2, 3, 4, 5];
   rating = 0;
   hoverState = 0;
+  activeStar: boolean = true;
 
 
   constructor(private uploadService: FileUploadService) { }
@@ -50,12 +51,10 @@ export class HashtagMaterialsComponent implements OnInit {
 
 onStarEnter(starId: any) {
   this.hoverState = starId;
-  console.log(this. hoverState);
 }
 
 onStarLeave() {
   this.hoverState = 0;
-  console.log(this.hoverState);
 }
 
 onStarClicked(starId: any) {
