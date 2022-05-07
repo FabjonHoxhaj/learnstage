5f032bfe099aa15e37dfe038e96a08570034f003
Fabjon Hoxhaj <fahox1991@gmail.com>
2022-05-07 19:43:42 +0200

    component created for menu to tag files

---
+++ learnstage/src/app/hamburger-tag/hamburger-tag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger-tag',
+  templateUrl: './hamburger-tag.component.html',
+  styleUrls: ['./hamburger-tag.component.css']
+})
+export class HamburgerTagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
